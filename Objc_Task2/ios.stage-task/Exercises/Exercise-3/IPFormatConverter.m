846621f2a6f15f566614b1fc65c4b2c91c44cb43
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-17 22:22:49 +0300

    feat: adding new code in exercise three

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,34 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
-}
-
+    if (numbersArray.count == 0) {
+           return @"";
+       }
+    for (id i in numbersArray) {
+        if ([i intValue] < 0){
+            return @"Negative numbers are not valid for input.";
+        }
+    
+    if ([i intValue] > 255) {
+        return @"The numbers in the input array can be in the range from 0 to 255.";
+    }
+    }
+       
+    NSMutableString *results = [NSMutableString stringWithString:@""];
+    
+    for (int b = 0; b < 4; b++) {
+            if (b < numbersArray.count) {
+                [results appendFormat:@"%@", [[numbersArray objectAtIndex:b] stringValue]];
+            }else {
+                [results appendString:@"0"];
+            }
+        if (b < 3) {
+            [results appendString:@"."];
+        
+        }
+    
+    }
+    return results;
+        
+    }
 @end
