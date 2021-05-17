33a208bc14de7f347e7c79cb0678a3c732773813
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-18 01:09:41 +0300

    feat: adding new code in exercise four

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,29 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    if([array count] == 0 || array == nil || ![array[0] isKindOfClass:[NSArray class]]){
+        return @[];
+    }
+    
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    NSMutableArray *stringArray = [[NSMutableArray alloc] init];
+    
+    for (id b in array) {
+        for (id a in b){
+        if ([b isKindOfClass:NSString.class]){
+            [stringArray addObject: a];
+        } else {
+            [numberArray addObject:a];
+        }
+    }
+    }
+        
+        [numberArray sortUsingSelector:@selector(compare:)];
+        [stringArray sortUsingSelector:@selector(compare:)];
+        
+        if (numberArray.count == 0) return stringArray;
+        if (stringArray.count == 0) return numberArray;
+            
+        return [NSArray arrayWithObjects: numberArray, stringArray, nil];
 }
-
 @end
