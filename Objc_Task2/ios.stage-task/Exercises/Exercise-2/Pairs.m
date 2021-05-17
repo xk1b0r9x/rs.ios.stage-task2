5b6bbf5b90eaf91d827902d8ed5a406b47b8140a
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-17 19:12:46 +0300

    feat: adding new code in exercise two

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,22 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger countPairs = 0;
+    
+    if (array.count == 0 || array.count == 1) {
+        return 0;
+    }
+    for (int a = 0; a < (array.count - 1); a++) {
+        NSInteger j = [[array objectAtIndex:a] intValue];
+    
+    for (int b = a + 1; b < (array.count); b++) {
+        NSInteger c = [[array objectAtIndex:b] intValue];
+        if (c == j + [number intValue]) {
+                countPairs ++;
+            }
+        }
+    }
+    
+    return countPairs;
 }
-
 @end
