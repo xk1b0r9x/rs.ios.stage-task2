3ad870fe601a0383c07866bd6a35bb1c76c378da
Vladimir S <46943806+xk1b0r9x@users.noreply.github.com>
2021-05-17 18:30:01 +0300

    feat: add new code

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,14 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+     UInt8 results = 0;
+    
+    for (int a = 0; a < 8; ++a) {
+        results = results << 1;
+        if ((n & 1) == 1){
+        results = results ^ 1;
+        }
+       n = n >> 1;
+    }
+    return results;
 }
